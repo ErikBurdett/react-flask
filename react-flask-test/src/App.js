53ddfdfaba10fca0866533182b3145c888cffc53
ErikBurdett <erikaburdet@gmail.com>
2021-08-30 23:42:48 -0500

    refactored article list into ArticleList Component taking props

--- react-flask-test/src/App.js
+++ react-flask-test/src/App.js
@@ -1,32 +1,38 @@
 import React, {useEffect, useState} from 'react'
 import './App.css'
 import {Articles} from './components/Articles'
+import ArticleList from './components/ArticleList'
 import {ArticleForm} from './components/ArticleForm'
 import {Details} from './components/Details'
 import {Container} from "semantic-ui-react"
 
 function App() {
 
-  const [articles, setArticles] = useState([]);
+  const [articles, setArticles] = useState([])
 
   useEffect(()=>{
-    fetch('/get')
-    .then(response=>response.json()
-    .then(data =>{
-      setArticles(data);
-    }))
+    fetch('/get', {
+      'method':'GET',
+      headers: {
+        'Content-Type': 'application/json'}
+    }
+  )
+  .then(response=>response.json())
+  .then(response =>setArticles(response))
+  .catch(error => console.log(error))
 
   }, [])
 
 
   return (
     <div className="App">
-      <Container style={{marginTop: 40}}>
-        <ArticleForm onNewArticle={article => setArticles(currentArticles=>[...currentArticles, article])}/>
-        <Articles articles = {articles}/>
-        {/* <Details/> */}
-      </Container>
+      <h1>Flask and React</h1>
+      <ArticleList articles ={articles}/>
+      <div>
+      <ArticleForm/>
     </div>
+    </div>
+    
   )
 }
 
