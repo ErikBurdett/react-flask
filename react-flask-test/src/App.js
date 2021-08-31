4b3b5f9f0dc6d1fa3e99b49700516bb61184ec75
ErikBurdett <erikaburdet@gmail.com>
2021-08-31 10:21:31 -0500

    added delete request - all current requests functioning correctly

--- react-flask-test/src/App.js
+++ react-flask-test/src/App.js
@@ -1,14 +1,13 @@
 import React, {useEffect, useState} from 'react'
 import './App.css'
-import {Articles} from './components/Articles'
 import ArticleList from './components/ArticleList'
-import {ArticleForm} from './components/ArticleForm'
-import {Details} from './components/Details'
-import {Container} from "semantic-ui-react"
+import Form from './components/Form'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
 
   const [articles, setArticles] = useState([])
+  const [editedArticle, setEditedArticle] = useState(null)
 
   useEffect(()=>{
     fetch('/get', {
@@ -23,14 +22,65 @@ function App() {
 
   }, [])
 
+  const editArticle = (article) =>{
+    setEditedArticle(article)
+
+  }
+
+  const updatedData = (article) =>{
+    const new_article = articles.map(my_article =>{
+      if(my_article.id == article.id){
+        return article
+      } else {
+        return my_article
+      }
+    })
+    setArticles(new_article)
+  }
+
+  const openForm =() =>{
+    setEditedArticle({title: '', body: ''})
+  }
+
+  const insertedArticle =(article) =>{
+    const new_articles = [...articles, article]
+    setArticles(new_articles)
+  }
+
+  const deleteArticle = (article) => {
+    const new_articles = articles.filter(myarticle => {
+      if(myarticle.id === article.id){
+        return false;
+      } 
+      return true
+    })
+    setArticles(new_articles)
+  }
+
 
   return (
     <div className="App">
-      <h1>Flask and React</h1>
-      <ArticleList articles ={articles}/>
-      <div>
-      <ArticleForm/>
-    </div>
+      <div className ="row">
+        <div className="col">
+        <h1>Flask and React</h1>
+          
+        </div>
+        <div className="col">
+        <button
+        className="btn btn-success"
+        onClick={openForm}
+        >
+          Create Article
+        </button>
+          
+        </div>
+      </div>
+      
+      <br/>
+      <br/>
+      <ArticleList articles ={articles} editArticle={editArticle} deleteArticle = {deleteArticle}/>
+
+      {editedArticle ? <Form article = {editedArticle} updatedData={updatedData} insertedArticle = {insertedArticle}/> : null }
     </div>
     
   )
--- react-flask-test/src/components/ArticleList.js
+++ react-flask-test/src/components/ArticleList.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import APIService from './APIService'
 
 function ArticleList(props) {
-    return (<div>
+
+    const editArticle = article =>{
+        props.editArticle(article)
+    }
+
+    const deleteArticle = (article) =>{
+        APIService.DeleteArticle(article.id)
+        .then(()=>props.deleteArticle(article))
+    }
+    return (
+        <div>
         {props.articles && props.articles.map(article =>{
           return (
             <div key ={article.id}>
@@ -10,10 +21,25 @@ function ArticleList(props) {
               </h2>
               <p>{article.body}</p>
               <p>{article.date}</p>
+              <div className="row">
+                  <div className = "col-md-1">
+                      <button className = "btn btn-primary"
+                      onClick = {()=> editArticle(article)}>
+                        update
+                      </button>
+                  </div>
+                  <div className = "col">
+                      <button className = "btn btn-danger"
+                      onClick={()=> deleteArticle(article)}>
+                        delete
+                      </button>
+                  </div>
+            </div>
+            <hr/>
             </div>
           )
         })}
-      </div>
+        </div>
     )
 }
 
